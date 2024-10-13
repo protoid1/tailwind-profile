61c5c55fbc567469b1163b82ae21432e4ebc88ab
protoid1 <randiardiansyah90@gmail.com>
2024-10-13 14:28:43 +0700

    project pertama

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'krem': '#FCFAEE',
+        'merona': '#B8001F',
+      }
+    },
+  },
+  plugins: [],
+}
+
